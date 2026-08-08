31b6a020f5ba1329a32b584290eface8ee20253e
MisaAndrejezieski <165926051+MisaAndrejezieski@users.noreply.github.com>
2026-08-08 10:32:51 -0300

    Update LoginActivity.kt

--- app/src/main/java/com/example/profiss/LoginActivity.kt
+++ app/src/main/java/com/example/profiss/LoginActivity.kt
@@ -3,25 +3,25 @@ package com.example.profiss
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast // <--- Importante: Importei a biblioteca do Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Isso liga o código ao seu layout XML
         setContentView(R.layout.activity_login)
 
         // 1. Botão de Voltar (a seta no topo esquerdo)
         val btnBack = findViewById<ImageView>(R.id.btnBack)
         btnBack.setOnClickListener {
-            // finish() simplesmente fecha essa tela e volta para a anterior
             finish()
         }
 
-        // 2. Botão de Entrar
+        // 2. Botão de Entrar (AGORA COM AÇÃO!)
         val btnLoginAction = findViewById<Button>(R.id.btnLoginAction)
         btnLoginAction.setOnClickListener {
-            // Aqui você vai colocar a lógica de login depois
+            // Isso aqui vai aparecer flutuando na tela do seu celular
+            Toast.makeText(this, "Login clicado! Em breve...", Toast.LENGTH_SHORT).show()
         }
     }
 }
